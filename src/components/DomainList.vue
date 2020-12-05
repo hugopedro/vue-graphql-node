418d0540ce038ec50dd7cf827d9509e716eb6cab
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-12-05 16:09:47 -0300

    Schema, Query e Resolvers

--- src/components/DomainList.vue
+++ src/components/DomainList.vue
@@ -25,6 +25,7 @@
 
 <script>
 
+import axios from "axios/dist/axios";
 import AppItemList from './AppItemList.vue';
 
 export default {
@@ -35,8 +36,8 @@ export default {
   },
 
   data: () => ({
-    prefixes: ['Air', 'Jet', 'Flight'],
-    sufixes: ['Hub', 'Station', 'Mart']
+    prefixes: [],
+    sufixes: []
   }),
   methods: {
     addPrefix(prefix) {
@@ -67,6 +68,30 @@ export default {
       }
       return domains;
     }
+  },
+  created() {
+    axios({
+      url: "http://localhost:4000",
+      method: "post",
+      data: {
+        query: `
+        {
+          prefixes: items (type: "prefix") {
+            id
+            type
+            description
+          }
+          sufixes: items (type: "sufix") {
+            description
+          }
+        }
+        `
+      }
+    }).then(response => {
+      const query = response.data;
+      this.prefixes = query.data.prefixes.map(prefix => prefix.description);
+      this.sufixes = query.data.sufixes.map(sufix => sufix.description);
+    })
   }
 }
 </script>
